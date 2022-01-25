553737bd4cf892092ecd6f651562cdf198c8f640
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-01-25 13:10:41 +0530

    leetcode

---
+++ Leetcode/46_Permutations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permutation(vector<int> nums, int idx, vector<vector<int>> &ans)
+{
+    if (idx >= nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for (int j = idx; j < nums.size(); j++)
+    {
+        swap(nums[idx], nums[j]);
+        permutation(nums, idx + 1, ans);
+        swap(nums[idx], nums[j]);
+    }
+}
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    permutation(nums, 0, ans);
+    return ans;
+}
