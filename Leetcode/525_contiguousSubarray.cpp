b4962d8c55ca40258f3ee6b8c54799b657dea8e6
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-02-04 22:15:20 +0530

    ..

---
+++ Leetcode/525_contiguousSubarray.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMaxLength(vector<int> &nums)
+{
+    int maxLen = 0, sum = 0;
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++)
+        if (nums[i] == 0)
+            nums[i] = -1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        if (sum == 0)
+            maxLen = i + 1;
+        else
+        {
+            if (map.find(sum) != map.end())
+                maxLen = max(maxLen, i - map[sum]);
+            else
+                map[sum] = i;
+        }
+    }
+    return maxLen;
+}
