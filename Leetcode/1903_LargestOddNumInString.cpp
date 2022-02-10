95a3fe9126482bbac84f3919b6bbd1d6a3c80e6e
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-02-11 03:59:33 +0530

    lc

---
+++ Leetcode/1903_LargestOddNumInString.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string largestOddNumber(string num)
+{
+    for (int i = num.length() - 1; i >= 0; i--)
+    {
+        if (num[i] % 2 == 1)
+            return num.substr(0, i + 1);
+    }
+    return "";
+}
