4325ef073df5c6f2a30a4e9aa87f18e70f697af6
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-02-22 14:03:53 +0530

    lc

---
+++ Leetcode/171_ExcelSheetColumn.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int titleToNumber(string columnTitle)
+{
+    int num = 0, n = columnTitle.length();
+    for (int i = n - 1; i >= 0; i--)
+    {
+        num += (int)(columnTitle[i] - 'A' + 1) * pow(26, n - i - 1);
+    }
+    return num;
+}
